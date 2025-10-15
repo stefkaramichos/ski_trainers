ca661f8e90f9ce9cb6f63de67e0d52d8d6217b9b
stefanos karamichos <stephanoskaramichos@gmail.com>
2025-10-15 14:17:40 +0300

    text commit for production env

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\MountainsController;
 use App\Http\Controllers\ProfilesController;
 use App\Http\Controllers\AdminTrainersController;
 use App\Http\Controllers\AvailabilityController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BookingController;
+//test
 
 Route::get('/', function () {
     return view('home');
@@ -13,7 +15,7 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('dashboard'); // or 'home.index'
 Route::get('/mountain/{mountain}', [App\Http\Controllers\MountainsController::class, 'mountain'])->name('mountain');
 Route::any('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'profile'])->name('profile');
 Route::any('/admin_trainers', [App\Http\Controllers\AdminTrainersController::class, 'admin_trainers'])->name('admin.trainers');
